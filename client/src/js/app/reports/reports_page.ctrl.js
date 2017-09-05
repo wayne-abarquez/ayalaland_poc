1a5d1f94440298576942ed04072f1658a44d66be
Wayne <wayne.abarquez@navagis.com>
2017-09-05 11:41:13 +0800

    added selection for report for admin and clau secretary

--- client/src/js/app/reports/reports_page.ctrl.js
+++ client/src/js/app/reports/reports_page.ctrl.js
@@ -8,10 +8,31 @@ angular.module('demoApp.reports')
         var vm = this;
 
         $rootScope.showReportResultsTable = true;
+        $rootScope.showReportResultsSelection = false;
+
+        vm.reportsType = [
+            {
+                label: 'Due Diligence Report - Legal',
+                value: 'ISSUES-LEGAL'
+            },
+            {
+                label: 'Due Diligence Report - Technical',
+                value: 'ISSUES-TECHNICAL'
+            },
+            {
+                label: 'Landbank Inventory Report via GIS',
+                value: 'LOTS'
+            },
+            {
+                label: 'Landbank Inventory Analytics',
+                value: 'SBU'
+            }
+        ];
 
         initialize();
 
         vm.showReportsTable = showReportsTable;
+        vm.reportTypeChanged = reportTypeChanged;
 
         function initialize() {
             gmapServices.createMap('map-canvas');
@@ -25,6 +46,29 @@ angular.module('demoApp.reports')
                 var lotId = $(this).data('lot-id');
                 lotService.showReportIssueModal(lotId);
             });
+
+            $rootScope.$watch('currentUser', function (newValue) {
+                if (!newValue) return;
+
+                $rootScope.showReportResultsSelection = (['ADMIN', 'CLAU SECRETARY'].indexOf(newValue.role) > -1);
+                if ($rootScope.showReportResultsSelection) {
+                    $rootScope.showReportResultsTable = false;
+                }
+            });
+        }
+
+        function reportTypeChanged (report) {
+            var result = report.split('-');
+
+            var params = {
+                type: result[0]
+            };
+
+            if (result.length > 1) params.role = result[1];
+
+            $rootScope.showReportResultsTable = true;
+
+            $rootScope.$broadcast('report-type-changed', params);
         }
 
         function showReportsTable () {
--- client/src/js/app/reports/reports_result_table.ctrl.js
+++ client/src/js/app/reports/reports_result_table.ctrl.js
@@ -23,49 +23,73 @@ angular.module('demoApp.home')
         initialize();
 
         function initialize () {
+            $rootScope.$on('report-type-changed', function (e, params) {
+                switch(params.type) {
+                    case 'ISSUES':
+                        initIssuesReport(params);
+                        break;
+                    case 'LOTS':
+                        initCLAUReport();
+                        break;
+                    case 'SBU':
+                        initCAReport();
+                        break;
+                }
+            });
+
+
             $rootScope.$watch('currentUser', function (newValue) {
                 if (!newValue) return;
 
                 if (['LEGAL', 'MDC'].indexOf(newValue.role) > -1) {
-                    issuesService.getIssuesByUserRole(newValue.role)
-                        .then(function(issues){
-                            vm.report.title = 'LEGAL' == newValue.role ? 'Legal Issues' : 'Technical Issues';
-                            vm.report.headers = ['lot_offer_no', 'project_name', 'date_reported', 'description', 'action_item', 'status'];
-                            vm.report.dataType = 'ISSUES';
-
-                            if (issues.length > 0) {
-                                lotService.loadLots();
-                                vm.report.result = angular.copy(issues).map(function (item) {
-                                    return Object.flatten(item);
-                                });
-                                return;
-                            }
-                        });
+                    initIssuesReport(newValue);
                 } else if (newValue.role == 'C&A') {
-                    vm.report.title = 'Landbank Inventory Analytics';
-                    vm.report.headers = ['total_land_value', 'total_value_of_ali_owned', 'gfa'];
-                    vm.report.dataType = 'SBU';
-
-                    vm.filter.sbuSelection = ['ALL'].concat(SBU_SELECTION);
-
-                    lotService.loadLots();
-
-                    sbuChanged('ALL');
-
+                    initCAReport();
                 } else if (newValue.role == 'CLAU ANALYST') {
-                    vm.report.title = 'Landbank Inventory Report via GIS';
-                    vm.report.headers = ['lot_offer_no', 'estate_name', 'project_name', 'complete_address', 'sbu', 'lot_status'];
-                    vm.report.dataType = 'LOTS';
-
-                    lotService.getLandBankDataViaGIS()
-                        .then(function(result){
-                            vm.report.result = angular.copy(result);
-                            vm.report.result.forEach(function(item){
-                               lotService.addLot(item, true);
-                            });
-                        });
+                    initCLAUReport();
                 }
             });
+        }
+
+        function initCLAUReport () {
+            vm.report.title = 'Landbank Inventory Report via GIS';
+            vm.report.headers = ['lot_offer_no', 'estate_name', 'project_name', 'complete_address', 'sbu', 'lot_status'];
+            vm.report.dataType = 'LOTS';
+
+            lotService.getLandBankDataViaGIS()
+                .then(function (result) {
+                    vm.report.result = angular.copy(result);
+                    vm.report.result.forEach(function (item) {
+                        lotService.addLot(item, true);
+                    });
+                });
+        }
+
+        function initCAReport () {
+            vm.report.title = 'Landbank Inventory Analytics';
+            vm.report.headers = ['total_land_value', 'total_value_of_ali_owned', 'gfa'];
+            vm.report.dataType = 'SBU';
+            vm.filter.sbuSelection = ['ALL'].concat(SBU_SELECTION);
+
+            lotService.loadLots();
+            sbuChanged('ALL');
+        }
+
+        function initIssuesReport (newValue) {
+            issuesService.getIssuesByUserRole(newValue.role)
+                .then(function (issues) {
+                    vm.report.title = 'LEGAL' == newValue.role ? 'Legal Issues' : 'Technical Issues';
+                    vm.report.headers = ['lot_offer_no', 'project_name', 'date_reported', 'description', 'action_item', 'status'];
+                    vm.report.dataType = 'ISSUES';
+
+                    if (issues.length > 0) {
+                        lotService.loadLots();
+                        vm.report.result = angular.copy(issues).map(function (item) {
+                            return Object.flatten(item);
+                        });
+                        return;
+                    }
+                });
         }
 
         function sbuChanged (sbu) {
